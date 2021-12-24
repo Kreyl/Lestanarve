07670e72a5396a08e6c7ef51ba7317c194b212f3
kreyl <laystoll@yandex.ru>
2021-12-25 01:03:11 +0300

    + UART works

---
+++ Lestanarve_fw/EvtMsgIDs.h
@@ -0,0 +1,30 @@
+/*
+ * EvtMsgIDs.h
+ *
+ *  Created on: 21 ���. 2017 �.
+ *      Author: Kreyl
+ */
+
+#pragma once
+
+enum EvtMsgId_t {
+    evtIdNone = 0, // Always
+
+    // Pretending to eternity
+    evtIdShellCmdRcvd,
+    evtIdEverySecond,
+
+    // Usb
+    evtIdUsbConnect,
+    evtIdUsbDisconnect,
+    evtIdUsbReady,
+    evtIdUsbNewCmd,
+    evtIdUsbInDone,
+    evtIdUsbOutDone,
+
+    // Misc periph
+    evtIdAcc,
+    evtIdPwrOffTimeout,
+
+    // App specific
+};
---
+++ Lestanarve_fw/main.cpp
@@ -0,0 +1,106 @@
+#include "board.h"
+#include "ch.h"
+#include "hal.h"
+#include "uart2.h"
+#include "led.h"
+#include "Sequences.h"
+#include "radio_lvl1.h"
+#include "kl_lib.h"
+#include "MsgQ.h"
+#include "acg_lsm6ds3.h"
+#include "SimpleSensors.h"
+
+#if 1 // ======================== Variables and defines ========================
+// Forever
+EvtMsgQ_t<EvtMsg_t, MAIN_EVT_Q_LEN> EvtQMain;
+static const UartParams_t CmdUartParams(256000, CMD_UART_PARAMS);
+CmdUart_t Uart{CmdUartParams};
+static void ITask();
+static void OnCmd(Shell_t *PShell);
+
+// ==== Periphery ====
+LedSmooth_t Lumos { LED_PIN };
+
+// ==== Timers ====
+//static TmrKL_t TmrEverySecond {MS2ST(1000), evtIdEverySecond, tktPeriodic};
+//static int32_t TimeS;
+#endif
+
+int main(void) {
+    // ==== Init Vcore & clock system ====
+    Clk.SetCoreClk(cclk16MHz);
+//    Clk.SetCoreClk(cclk48MHz);
+    Clk.UpdateFreqValues();
+
+    // === Init OS ===
+    halInit();
+    chSysInit();
+    EvtQMain.Init();
+
+    // ==== Init hardware ====
+    Uart.Init();
+    Printf("\r%S %S\r", APP_NAME, XSTRINGIFY(BUILD_TIME));
+    Clk.PrintFreqs();
+
+//    Acg.Init();
+
+//    Lumos.Init();
+//    Lumos.StartOrRestart(lsqLStart);
+
+//    SimpleSensors::Init();
+//    Adc.Init();
+
+    // ==== Time and timers ====
+//    TmrEverySecond.StartOrRestart();
+
+    // ==== Radio ====
+//    if(Radio.Init() == retvOk) Led.StartOrRestart(lsqStart);
+//    else Led.StartOrRestart(lsqFailure);
+//    chThdSleepMilliseconds(1008);
+
+    // Main cycle
+    ITask();
+}
+
+__noreturn
+void ITask() {
+    while(true) {
+        EvtMsg_t Msg = EvtQMain.Fetch(TIME_INFINITE);
+        switch(Msg.ID) {
+//            case evtIdEverySecond:
+//                TimeS++;
+//                ReadAndSetupMode();
+//                break;
+
+            case evtIdShellCmdRcvd:
+                while(((CmdUart_t*)Msg.Ptr)->TryParseRxBuff() == retvOk) OnCmd((Shell_t*)((CmdUart_t*)Msg.Ptr));
+                break;
+            default: Printf("Unhandled Msg %u\r", Msg.ID); break;
+        } // Switch
+    } // while true
+} // ITask()
+
+void ProcessCharging(PinSnsState_t *PState, uint32_t Len) {
+
+}
+
+#if 1 // ================= Command processing ====================
+void OnCmd(Shell_t *PShell) {
+	Cmd_t *PCmd = &PShell->Cmd;
+    __attribute__((unused)) int32_t dw32 = 0;  // May be unused in some configurations
+//    Uart.Printf("%S\r", PCmd->Name);
+    // Handle command
+    if(PCmd->NameIs("Ping")) {
+        PShell->Ok();
+    }
+    else if(PCmd->NameIs("Version")) PShell->Print("%S %S\r", APP_NAME, XSTRINGIFY(BUILD_TIME));
+
+    else if(PCmd->NameIs("lum")) {
+        uint8_t Brt;
+        if(PCmd->GetNext<uint8_t>(&Brt) != retvOk) { PShell->CmdError(); return; }
+        Lumos.SetBrightness(Brt);
+    }
+
+    else PShell->CmdUnknown();
+}
+#endif
